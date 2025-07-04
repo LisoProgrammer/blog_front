d1bf9fdd5adb4259e6002f2aea1141fcb918ea89
Lisandro Zapata <zlisandro5@gmail.com>
2025-07-04 10:32:41 -0500

    Por defecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ArticuloComponent } from "./articulo/articulo.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ArticuloComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
