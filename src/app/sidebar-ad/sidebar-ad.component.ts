664e6733d2ee40b848c43aee67e80cb6b64c607d
Lisandro Zapata <zlisandro5@gmail.com>
2025-07-04 10:28:36 -0500

    Componente de anuncios en aside

---
+++ src/app/sidebar-ad/sidebar-ad.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-ad',
+  imports: [],
+  templateUrl: './sidebar-ad.component.html',
+  styleUrl: './sidebar-ad.component.css'
+})
+export class SidebarAdComponent {
+
+}
