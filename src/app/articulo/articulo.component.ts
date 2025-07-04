071cb7505de873514c3ff79657ccd0d2b73bc63a
Lisandro Zapata <zlisandro5@gmail.com>
2025-07-04 15:07:19 -0500

    Maquetado inicial SEO

--- src/app/articulo/articulo.component.ts
+++ src/app/articulo/articulo.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { SidebarAdComponent } from "../sidebar-ad/sidebar-ad.component";
+import { NavbarBlogComponent } from "../navbar-blog/navbar-blog.component";
 
 @Component({
   selector: 'app-articulo',
-  imports: [],
+  imports: [FooterComponent, SidebarAdComponent, NavbarBlogComponent],
   templateUrl: './articulo.component.html',
   styleUrl: './articulo.component.css'
 })
